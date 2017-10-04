d0037a91ba349c4e559bcdf30cd1ec929d0edd58
Leon Hartley <leonhartley@ekmsystems.co.uk>
2017-10-04 08:22:20 +0100

    More API design

---
+++ Hades/Hades.API/Services/Rooms/IRoomService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hades.API.Services.Rooms
+{
+    public interface IRoomService : IService
+    {
+    }
+}
--- Hades/Hades.Items/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hades.Items
-{
-    public class Class1
-    {
-    }
-}
