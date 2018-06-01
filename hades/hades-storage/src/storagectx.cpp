e3b1f727e2a39a24ab3a5a124f590d483654e645
Leon Hartley <leonhartley@ekmsystems.co.uk>
2018-06-01 20:07:29 +0100

    Use my fork :D

--- hades/hades-storage/src/storagectx.cpp
+++ hades/hades-storage/src/storagectx.cpp
@@ -5,14 +5,21 @@
 #include <storage/storagectx.h>
 #include <common/log/log.h>
 
+#include <boost/shared_ptr.hpp>
 #include "MySQLConnection.h"
 
 using namespace active911;
 
 void hades::StorageCtx::test() {
     // Create a pool of 5 MySQL connections
-    shared_ptr<MySQLConnectionFactory>mysql_connection_factory(new MySQLConnectionFactory("localhost","root","mysql_password"));
-    shared_ptr<ConnectionPool<MySQLConnection> >mysql_pool(new ConnectionPool<MySQLConnection>(5, mysql_connection_factory));
+    std::shared_ptr<MySQLConnectionFactory> mysql_connection_factory(
+            new MySQLConnectionFactory("localhost", "root", ""));
 
+    std::shared_ptr<ConnectionPool<MySQLConnection>> mysql_pool(
+            new ConnectionPool<MySQLConnection>(5, mysql_connection_factory));
+
+    auto connection = mysql_pool->borrow();
+
+    connection->sql_connection->setSchema("cometsrv");
 
 }
