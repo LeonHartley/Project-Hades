2b28298bc1ad710095c507a2f3ad887f378dd5ac
Leon Hartley <leonhartley@ekmsystems.co.uk>
2018-06-02 00:56:37 +0100

    More buffer stuff

--- hades/hades-common/src/net/buffer.cpp
+++ hades/hades-common/src/net/buffer.cpp
@@ -2,8 +2,10 @@
 
 #include <string>
 
+using namespace hades;
+
 template<>
-int hades::Buffer::read() {
+int Buffer::read() {
     return (0xff & this->buffer_[this->readerIndex_++] << 24) |
            (0xff & this->buffer_[this->readerIndex_++] << 16) |
            (0xff & this->buffer_[this->readerIndex_++] << 8) |
@@ -11,24 +13,47 @@ int hades::Buffer::read() {
 }
 
 template<>
-void hades::Buffer::write(std::string data) {
-
-}
-
-template<>
-short hades::Buffer::read() {
+short Buffer::read() {
     return (0xff & this->buffer_[this->readerIndex_++]) << 8 |
            (0xff & this->buffer_[this->readerIndex_++] << 0);
 }
 
 template<>
-std::string hades::Buffer::read() {
+std::string Buffer::read() {
     short len = this->read<short>();
 
     return std::string(this->buffer_ + this->readerIndex_, static_cast<unsigned long>(this->readerIndex_ += len));
 }
 
 template<>
-bool hades::Buffer::read() {
+bool Buffer::read() {
     return true;
+}
+
+template<>
+void Buffer::write(char data) {
+    this->buffer_[this->writerIndex_++] = data;
+}
+
+template<>
+void Buffer::write(short data) {
+    this->buffer_[this->writerIndex_++] = ((data >> 8) & 0xff);
+    this->buffer_[this->writerIndex_++] = (data & 0xff);
+}
+
+template<>
+void Buffer::write(int data) {
+    this->buffer_[this->writerIndex_++] = ((data >> 24) & 0xff);
+    this->buffer_[this->writerIndex_++] = ((data >> 16) & 0xff);
+    this->buffer_[this->writerIndex_++] = ((data >> 8) & 0xff);
+    this->buffer_[this->writerIndex_++] = (data & 0xff);
+}
+
+template<>
+void Buffer::write(std::string data) {
+    this->write<short>(static_cast<short>(data.length()));
+
+    for(char &character : data) {
+        this->write<char>(character);
+    }
 }
