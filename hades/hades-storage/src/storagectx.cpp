e9971a8fbb0ff80c7c547c7708c1ea5cac58945c
Leon Hartley <leonhartley@ekmsystems.co.uk>
2018-06-01 19:35:33 +0100

    FindMySQLClient & add connection pool library

--- hades/hades-storage/src/storagectx.cpp
+++ hades/hades-storage/src/storagectx.cpp
@@ -5,6 +5,14 @@
 #include <storage/storagectx.h>
 #include <common/log/log.h>
 
+#include "MySQLConnection.h"
+
+using namespace active911;
+
 void hades::StorageCtx::test() {
-    LOG(INFO) << "Hi! :D";
+    // Create a pool of 5 MySQL connections
+    shared_ptr<MySQLConnectionFactory>mysql_connection_factory(new MySQLConnectionFactory("localhost","root","mysql_password"));
+    shared_ptr<ConnectionPool<MySQLConnection> >mysql_pool(new ConnectionPool<MySQLConnection>(5, mysql_connection_factory));
+
+
 }
